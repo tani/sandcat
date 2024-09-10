cc61c0968d5c3a3530f4993f1c6c2e2cef7fe2fe
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-09-10 06:57:49 +0000

    Update tests

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ A, B, C = Atom("A"), Atom("B"), Atom("C")
 
 class TestSandcat(unittest.TestCase):
     def test_case_1(self):
-        self.assertFalse(check([C << A, B, A], C, algorithm="z3"))
+        self.assertFalse(check([C << A, B, A], C))
 
     def test_case_2(self):
-        self.assertTrue(check([C << A, A, B], C, algorithm="z3"))
+        self.assertTrue(check([C << A, A, B], C))
 
     def test_case_3(self):
-        self.assertTrue(check([C << A, A, C >> C], C, algorithm="z3"))
+        self.assertTrue(check([C << A, A, C >> C], C))
 
 
 if __name__ == "__main__":
